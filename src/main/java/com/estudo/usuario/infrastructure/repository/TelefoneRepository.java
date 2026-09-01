8655eabeacd2ea89a690560d51e7ee0b6a44445c
Lucas <lucas20azevedo26@gmail.com>
2026-08-31 21:35:31 -0300

    adicionadas classes de security

---
+++ src/main/java/com/estudo/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.usuario.infrastructure.repository;
+
+import com.aprendendospring.estudo.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
